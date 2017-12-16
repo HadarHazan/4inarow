0fbebfff6dbea5b12058de03a6af942d7280d9ef
HadarHazan <hhadar88@gmail.com>
2017-12-16 15:43:04 +0200

    Add mongoDB

--- app.js
+++ app.js
@@ -1,127 +1,70 @@
 var express = require('express')
-var app = express()
 var bodyParser = require('body-parser')
-
-//var http = require("http");
-
- //Retrieve
-var MongoClient = require('mongodb').MongoClient
-  
-const initServerAfterDb = function(err, db) {
-  if(!err) {
-      
-        console.log("We are connected")
-        var collection = db.collection('mycollection')
-
-        app.use(bodyParser.json());
-
-        app.use('/static', express.static('public'))
-
-        app.get('/', function (req, res) {
-          res.send('Hello World!')
-        })
-
-        app.post('/winner', (req, res) => {
-            var d = new Date()
-           
-            console.log("body is", req.body, d.toUTCString())
-            var doc = {
-                winnerPlayer: req.body,
-                date: "hadar"
-            }
-            collection.insert(doc)
-        })
-        
-        app.get('/board', (req, res) => {           
-            
-            var winnerList = collection.find()
-            
-            console.log("num of doc", collection.find())
-            
-             // Retry to get the collection, should work as it's now created
-            db.collections(function(err, collections) {
-              assert.equal(null, err)
-              assert.ok(collections.length > 0)
-
-              db.close()
-            })
-        })
-                           
-
-        app.listen(3000, function () {
-            console.log('Example app listening on port 3000!')
-        })
-
-//        db.close();
-//        console.log("We are disconnected")
-  }
-}
-
-// Connect to the db
-MongoClient.connect("mongodb://localhost:27017/4inarow", initServerAfterDb)  
-
-//app.use('/static', express.static('public'))
-//
-//app.get('/', function (req, res) {
-//  res.send('Hello World!')
-//})
-//
-//app.listen(3000, function () {
-//    console.log('Example app listening on port 3000!')
-//})
-//var mongo = require('mongodb'),
-// Server = mongo.Server,
-// Db = mongo.Db;
-//
-//var server = new Server('localhost', 27017, {auto_reconnect: true});
-//var db = new Db('4inarow', server);
-//
-//var onErr = function(err,callback){
-// db.close();
-// callback(err);
-//};
-
-//app.get('/board', (req, res) => {           
-//    db.open(function(err, db) {
-//  if(!err) {
-//      console.log(db.defu
-//      console.log("We are connected")
-//      db.collection('mycollection', function(err, collection) {
-//    if(!err){
-//        console.log(collection.length)
-//        console.log(collection[0])
-//     collection.find().toArray(function(err, docs) {
-//      if(!err){
-//       console.log(docs.length)
-//       console.log(docs["<0>"])
-//       var intCount = docs.length;
-//       if(intCount > 0){
-//        var strJson = ""
-//        for(var i=0; i<intCount;){
-//         strJson += '{"player":"' + docs[i].winner + '"}'
-//         i=i+1;
-//         if(i<intCount){strJson+=','}
-//        }
-//        console.log(strJson)
-//        callback("",JSON.parse(strJson))
-//       }
-//      }
-//      else{onErr(err,callback)}
-//     });//end collection.find
-//    }
-//    else{onErr(err,callback)}
-//   });//end db.collection
-//  }
-//  else{onErr(err,callback)}
-// });// end db.open
-//})
-
-'use strict';
 var http = require('http');
-var port = process.env.PORT || 1337;
+var MongoClient = require('mongodb').MongoClient
+var db = require('./server/db')
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
-console.log('Server running at http://127.0.0.1 Jump :1337/');
+
+const allMessages =[]
+const sockets =[]
+const port = process.env.PORT || 38700
+
+const app = express();
+//app.use(express.static(path.join(__dirname, 'bower_components')));
+// const db = new Db('4inarow', new Server('localhost', 27017));
+
+const server = http.createServer(app, function (socket){
+  sockets.push(socket);
+    socket.on('data', function(d) {
+        for (var i = 0; i < sockets.length; i++){
+            if (sockets[i] == socket) continue;
+            sockets[i].write(d);
+            console.log(d)
+        }
+    });
+    
+    socket.on('end', function(d) {
+        var i = sockets.indexOf(socket);
+        sockets.splice(i, 1);
+    })
+})
+
+// Connect to Mongo on start
+db.connect('mongodb://localhost:27017/4inarow', function(err) {
+  if (err) {
+    console.log('Unable to connect to Mongo.')
+    process.exit(1)
+  } else {
+      console.log("We are connected");
+      server.listen(port, function() {
+        console.log('Express server running on *:' + port)
+    })
+  }
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'))
+
+app.post('/api/addMessage', function (req, res){
+    allMessages.push(req.body)
+})
+
+app.get('/api/getAllMessages', function (req, res){
+  res.send(allMessages)
+})
+
+app.post('/api/winner', function(req, res){
+    var collection = db.get().collection('score')
+    var d = new Date()
+    console.log("body is", req.body.player + " " + req.body.date)
+    var doc = {
+            Player: req.body.player,
+            date: req.body.date
+        }
+    collection.insert(doc)
+})
+
+app.get('*',function (req, res) {
+    res.redirect('score.html')
+})
---
+++ public/js/controllers/Game.js
@@ -0,0 +1,52 @@
+//Module
+define(function(require) {
+    class Game {  
+       constructor(player1, player2, m, v) {
+        this.player1 = player1
+        this.player2 = player2
+        //this.board = new Board(N,)
+        this.view = v
+        this.modal = m
+        this.view.addObserver(this);
+        this.currntPlayer = this.player1  
+      } 
+    };
+
+    Game.prototype.startGame = function() {
+        this.modal.initialize()
+        this.view.resetTable()
+        this.view.display(this.modal.boardArr)
+        this.view.setPlayer(this.currntPlayer)
+    }
+    
+    Game.prototype.play = function(i, j)
+    {
+        var winStatus = this.modal.checkWinner(i, j, this.currntPlayer)
+        this.view.updateStatus(winStatus)
+        if(winStatus){
+            $.post( "/api/winner", {player: this.currntPlayer, date: Date})
+                .done(function( data ) {
+                            alert( "Data Loaded: " + data );
+            });
+            this.startGame()
+        }
+        else {
+            this.switchPlayer()
+        }
+    }
+    
+    Game.prototype.switchPlayer = function() {
+        var player
+        if(this.currntPlayer === this.player1){
+            player = this.player2
+        } else {
+            player = this.player1
+        }
+        
+        this.currntPlayer = player
+        this.view.setPlayer(player)
+    }
+        
+    return Game
+})
+
